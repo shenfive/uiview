167c9d894b6420bee0ca711df925de2cb83cef66
Danny Shen <shenfive@gmail.com>
2019-11-16 10:47:06 +0800

    add view

--- uiview/ViewController.swift
+++ uiview/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var theView:UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
+        theView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        theView.center = self.view.center
+        theView.backgroundColor = UIColor.red
+        self.view.addSubview(theView)
+        
         
     }
 
