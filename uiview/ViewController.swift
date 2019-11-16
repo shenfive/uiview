d0e5185ac44c044500f683502955d1d5f0b10ddd
Danny Shen <shenfive@gmail.com>
2019-11-16 11:42:21 +0800

    rotate

--- uiview/ViewController.swift
+++ uiview/ViewController.swift
@@ -10,25 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var theView:UIView!
+  
     var timer:Timer!
     var counter = 0.0
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        theView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        theView.center = view.center
-        theView.backgroundColor = UIColor.red
-        theView.transform = CGAffineTransform(rotationAngle: 0)
-        view.addSubview(theView)
+        var newView = UIView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+        newView.center = view.center
+        newView.backgroundColor = UIColor.red
+        newView.transform = CGAffineTransform(rotationAngle: 0)
+        newView.layer.cornerRadius = 15
+        newView.clipsToBounds = true
+        
+        view.addSubview(newView)
         timer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { (timer) in
-            self.roteatView(targateView: self.theView)
+            self.roteatView(targateView: newView)
         })
     }
     
     func roteatView(targateView:UIView){
-        print("rotate")
         let a = counter * M_PI / 180
         targateView.transform = CGAffineTransform(rotationAngle: CGFloat(a))
         counter += 4
